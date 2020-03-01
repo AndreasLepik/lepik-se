63821e8e80c038b562af0632c5c66950495e8559
Wilhelm Wanecek <wilhelm@wanecek.com>
2020-03-01 15:31:08 +0100

    Set up gulp for building project

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { src, dest, parallel } = require('gulp');
+
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const htmlmin = require('gulp-htmlmin');
+const postcss = require('gulp-postcss');
+
+const paths = {
+  src: 'src',
+  dest: 'dist',
+};
+
+function html() {
+  return src(`${paths.src}/*.html`)
+    .pipe(htmlmin())
+    .pipe(dest(paths.dest));
+}
+
+function css() {
+  const plugins = [autoprefixer(), cssnano()];
+  return src(`${paths.src}/*.css`)
+    .pipe(postcss(plugins))
+    .pipe(dest(paths.dest));
+}
+
+function copy() {
+  return src(`${paths.src}/webbadoodle/*`)
+    .pipe(dest(`${paths.dest}/webbadoodle`));
+}
+
+exports.default = parallel(html, css, copy);
