ee41d30099f2a90b5ad5e73702c70f67310dacb0
AndreasLepik <andreas.lepik@live.com>
2020-03-15 17:42:06 +0100

    Fake actions now work from text input.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,44 @@
+// @ts-check
+const validActions = ['a0', 'a1', 'b0', 'b1'];
+
+const canvas = document.getElementById('canvas1');
+const context = canvas.getContext('2d');
+const textbox = document.getElementById("input1");
+const output = document.getElementById("outputText");
+
+document.getElementById('drawButton').addEventListener('click', readAction);
+
+initDraw();
+
 function initDraw() {
-    var canvas = document.getElementById("canvas1");
-    document.getElementById("title").innerHTML = 'texten har ändrats';
+    drawGrid();
+}
 
-    var context = canvas.getContext('2d');
+function drawGrid() {
+    context.strokeRect(0, 0, 225, 150);
+    context.strokeRect(0, 150, 225, 150);
+    context.strokeRect(225, 0, 225, 150);
+    context.strokeRect(225, 150, 225, 150);
+}
 
-    context.fillStyle = "#FF0000";
-    context.fillRect(1, 1, 100, 100);
-    context.moveTo(0,0);
-    context.lineTo(200, 100);
-    context.stroke(); 
-    console.log('initDraw');
+function readAction() {
+    var input = textbox.value.toLowerCase();
+    if (validActions.includes(input)) {
+        preformAction(input);
+    } else {
+        invalidAction(input);
+    }
+}
+
+function preformAction(input) {
+    context.fillRect(0,0,225,150);
+    output.innerHTML = "Action: " + input;
+}
+
+function invalidAction(input) {
+    output.innerHTML = "Invalid action: " + input;
+}
+
+function logEcho(text) {
+    console.log(text);
 }
