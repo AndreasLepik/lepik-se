b7ce8040ee335711d3d84723b649ea69670223b7
AndreasLepik <andreas.lepik@live.com>
2020-03-16 22:06:34 +0100

    Implemented basic grid drawing based on input. Added button press with Enter button.

--- src/main.js
+++ src/main.js
@@ -5,22 +5,32 @@ Script containing logic for the graphics page of lepik.se
 
 // Global variables
 
-var validActions = ['a0', 'a1', 'b0', 'b1'];
-const swRed = '#D00000';
+var validActions = ['help', 'a0', 'a1', 'b0', 'b1'];
 
+const numberOfRows =    2;
+const numberOfCols =    2;
+const swRed =           '#D00000';
+const swBlue =          '#0000D0';
+const swWhite =         '#D0D0D0';
 
 const canvas =          document.getElementById('canvas1');
 const canvasPosition =  canvas.getBoundingClientRect();
 const height =          canvasPosition.height;
 const width =           canvasPosition.width;
 const context =         canvas.getContext('2d');
-const textbox =         document.getElementById("input1");
+const textBox =         document.getElementById("input1");
 const output =          document.getElementById("outputText");
 
 
-// Link objects to functions
+// Link elements to functions
 
-document.getElementById('drawButton').addEventListener('click', readAction);
+document.getElementById("drawButton").addEventListener('click', readAction);
+textBox.addEventListener('keyup', function (event) {
+    if (event.keyCode == 13) {
+        event.preventDefault();
+        document.getElementById("drawButton").click();
+    }
+});
 
 
 // Functions to run on start up
@@ -31,6 +41,7 @@ initDraw();
 // Function definitions
 
 function initDraw() {
+    context.lineWidth = 4;
     context.fillStyle = '#202020';
     context.fillRect(0, 0, 450, 300);
     drawGrid();
@@ -38,14 +49,14 @@ function initDraw() {
 
 function drawGrid() {
     context.strokeStyle = '#FFFFFF';
-    context.strokeRect(0, 0, 225, 150);
-    context.strokeRect(0, 150, 225, 150);
-    context.strokeRect(225, 0, 225, 150);
-    context.strokeRect(225, 150, 225, 150);
+    context.strokeRect(2, 2, 225, 150);
+    context.strokeRect(2, 150, 225, 148);
+    context.strokeRect(225, 2, 223, 150);
+    context.strokeRect(225, 150, 223, 148);
 }
 
-function readAction() {
-    var input = textbox.value.toLowerCase();
+function readAction() { // Just a double check in this state
+    var input = textBox.value.toLowerCase();
     if (validActions.includes(input)) {
         performAction(input);
     } else {
@@ -54,13 +65,67 @@ function readAction() {
 }
 
 function performAction(input) {
+    switch (validActions.indexOf(input)) {
+        case 0: // help
+            writeToOutput("Wow, I love what you've done with your hair!"); // TODO
+            break;
+        case 1: case 2: case 3: case 4: // a0, a1, b0, b1
+            fillCell(input);
+            writeToOutput("Action: " + input);
+            break;
+        case -1: // Action not found
+            logEcho("This should never happen");
+            invalidAction(input);
+            break;
+        default:
+            invalidAction(input);
+            break;
+    }
+}
+
+function fillCell(gridPos) {
     context.fillStyle = swRed;
-    context.fillRect(1,1,223,148);
-    output.innerHTML = "Action: " + input;
+    var pos = getGridPos(gridPos);
+    context.fillRect(pos[0], pos[1], 20, 20);
+    writeToOutput("Action: " + gridPos);
+}
+
+function getGridPos(gridPos) {
+    var x = context.lineWidth;
+    var y = context.lineWidth;
+    switch (gridPos.charAt(0)) {
+        case "a":
+            break;
+        case "b":
+            x += ((width - 1.5) / numberOfCols);
+            break;
+        default:
+            console.error("Invalid x position");
+            break;
+    }
+    switch (gridPos.charAt(1)) {
+        case "0":
+            break;
+        case "1":
+            y += ((height - 1.5) / numberOfRows);
+            break;
+        default:
+            console.error("Invalid y position");
+            break;
+    }
+    return [x, y];
+}
+
+function getGridCellSize() {
+    
+}
+
+function writeToOutput(text) {
+    output.innerHTML = text;
 }
 
 function invalidAction(input) {
-    output.innerHTML = "Invalid action: " + input;
+    writeToOutput("Invalid action: " + input);
 }
 
 function logEcho(text) {
